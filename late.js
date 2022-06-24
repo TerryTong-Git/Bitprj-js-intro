39c8e1e498ae9c28bc6d9a77ca7da0457ca35ddf
terry tong <dooshtheboss1@gmail.com>
2022-06-24 17:57:42 +0800

    late

--- late.js
+++ late.js
@@ -1,1 +1,12 @@
 // start coding your function here!
+const date = new Date('December 17, 1995 03:24:00')
+function running_late (date) {
+    if (date.getHours()> 10) {
+        return "It is late!"}
+    else {
+        return "It is still early!"
+    }
+}
+
+exports.running_late = running_late;
+module.exports = running_late
