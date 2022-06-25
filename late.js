3b2a757fc13a7ca17f890c31e03988223224b62f
terry tong <dooshtheboss1@gmail.com>
2022-06-25 15:31:02 +0800

    try fix again

--- late.js
+++ late.js
@@ -1,14 +1,15 @@
 // start coding your function here!
 
 function running_late (date) {
-    date = new Date(date) 
+    const date1 = new Date(date) 
 
 
-    if (date.getHours()> 22) {
+    if (date1.getHours()> 22) {
         return "It is late!"}
     else {
         return "It is still early!"
     }
 }
+
 
 exports.running_late = running_late;
