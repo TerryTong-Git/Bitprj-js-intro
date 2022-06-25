10f6c9253448a597a5e86938e0cc78b739578592
terry tong <dooshtheboss1@gmail.com>
2022-06-25 15:36:43 +0800

    finally fixed

--- late.js
+++ late.js
@@ -2,14 +2,13 @@
 
 function running_late (date) {
     const date1 = new Date(date) 
+    
 
-
-    if (date1.getHours()> 22) {
+    if (date1.getHours()>= 22 && date1.getMinutes()> 1) {
         return "It is late!"}
     else {
         return "It is still early!"
     }
 }
-
 
 exports.running_late = running_late;
